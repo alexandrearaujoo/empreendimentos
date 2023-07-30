3434d2edb791db03c5b19ba11f74c5f76004b0e9
Alexandre <alexandre01393@gmail.com>
2023-07-30 15:42:35 -0300

    fix: fixing vercel.json

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,8 @@
 import server from 'json-server';
-import path from 'path';
 
 const jsonServer = server.create();
-const router = server.router(
-  path.join(__dirname, '..', 'enterprises-server.json')
-);
+const router = server.router('enterprises-server.json');
+
 const middlewares = server.defaults();
 
 jsonServer.use(middlewares);
