8d1a8dc08e2c73029ff55d9f3ddbe1a2924839e3
Alexandre <alexandre01393@gmail.com>
2023-07-30 14:49:00 -0300

    fix: removing async function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 import EnterpriseList from '@/components/EnterpriseList';
 
-import { getEnterprises } from '@/services/getEnterprises';
 import { Main } from '@/styles/pages/Home';
 
-export default async function Home() {
-  const { data } = await getEnterprises('1');
-
-  console.log(data);
-
+export default function Home() {
   return (
     <Main>
       <EnterpriseList />
