70fa65129138e5f358603183bc521564f9c2d903
Alexandre <alexandre01393@gmail.com>
2023-07-30 14:58:21 -0300

    feat: add server folder to json server

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import server from 'json-server';
+import path from 'path';
+
+const jsonServer = server.create();
+const router = server.router(
+  path.join(__dirname, '..', '..', 'enterprises-server.json')
+);
+const middlewares = server.defaults();
+
+jsonServer.use(middlewares);
+jsonServer.use(router);
+
+const PORT = 8000;
+
+jsonServer.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
